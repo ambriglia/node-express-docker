192e012f8398fb3de9dee643c1819750f45ecea9
Anthony Briglia <9729776+ambriglia@users.noreply.github.com>
2023-07-06 20:56:45 -0400

    update

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.use((req, res, next) => {
-  console.log('Hello from middleware!');
+  console.log('Hello from middleware');
   next();
 });
 
